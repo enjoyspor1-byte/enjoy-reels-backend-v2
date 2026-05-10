c1cf2be4d35ab37edc84c28df735edf685913075
enjoyspor1-byte <enjoyspor1@gmail.com>
2026-05-11 02:45:46 +0300

    Update server.js

--- server.js
+++ server.js
@@ -187,7 +187,7 @@ app.post("/render", upload.array("videos", 20), async (req, res) => {
   try {
     const perClip = clipLength(duration, files.length, cut);
 
-    for (let i = 0; i < files.length; i++) {
+    for (let i = 0; i < Math.min(files.length, 2); i++) {
       const file = files[i];
       const originalPath = file.path;
       const videoDuration = await getDuration(originalPath);
